dcaac03acb49b27e3ce2d1540ea0d688507d9d1f
harshmangal1108 <harsh.mangal1108@gmail.com>
2020-02-08 01:07:23 +0530

    Week 1

--- w106.R
+++ w106.R
@@ -11,5 +11,14 @@ pd=data.frame("name"=c("A","A","B","B"),
               "Bs"=c(141.2,139.3,135.2,160.1),
               "Bp"=c(90,78,80,81))
 ## 2 data frame
-pd_new=data.frame("Name"=c("A","C","B"),"Department"=c("PSE","Data Analysis","PSE"))
+pd_new=data.frame("name"=c("A","C","B"),"Department"=c("PSE","Data Analysis","PSE"))
 print(pd_new)
+
+### LEFT JOIN (rows from df2 to df1)
+pd_left.join<-left_join(pd,pd_new,by="name")
+### RIGHT JOIN (rows from df1 to df2)
+pd_right.join <- right_join(pd,pd_new,by="name")
+pd_right.join
+### INNER JOIN (retain and merges those rows with ids present in both dataframes)
+pd_inner.join = inner_join(pd,pd_new,by="name")
+pd_inner.join
